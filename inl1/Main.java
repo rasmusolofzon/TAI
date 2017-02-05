d83d6ed57697da580038de0338171177fae3c50a
Rasmus Olofzon <rasmus.olofzon@gmail.com>
2017-02-05 12:53:36 +0100

    CLI arguments are now updated and works. Example: Reversi w -t10500 mm play

--- inl1/Main.java
+++ inl1/Main.java
@@ -4,38 +4,53 @@ public class Main {
 
 		Board board = new Board();
 		BoardDrawer d = new BoardDrawer();
-		GameStrategy strat = new MinMaxStrategy(3);
+		GameStrategy strat = new MinMaxStrategy(4);
 
 		char playerColour = 'b';
 		int timeLimit = 10000; //milliseconds
+		String playMode = "auto";
+		String strategy = "ab";
 		
 		//CLI program setup
 		if (args.length > 0) {
 			try {
 				for (int i=0;i<args.length;i++) {
+					System.out.println(args[i]);
 					if (args[i].charAt(0) == '-') {
 						timeLimit = Integer.parseInt(args[i].substring(2, args[i].length()));
-					}
-					else {
+					} 
+					else if (args[i].charAt(0) == 'b' || args[i].charAt(0) == 'w') {
 						playerColour = args[i].charAt(0);
-						if (playerColour != 'b' && playerColour != 'w') throw new Exception();
+					} 
+					else if (args[i].equals("play") || args[i].equals("auto")) {
+						playMode = args[i];
+					}
+					else if (args[i].equals("ab") || args[i].equals("mm")) {
+						strategy = args[i];
+					} else {
+						throw new Exception();
 					}
 				}
 			} catch (Exception e) {
-				System.err.println("\nTwo possible CLI arguments:");
-				System.err.println("'b' for user to play as black, 'w' to play as white. No arg defaults to 'b'.");
-				System.err.println("'-tXXXX' to specify the time limit. XXXX is measured in milliseconds.");
-				System.err.println("Example: Reversi b -t10500\n");
+				System.err.println("\nFour possible CLI arguments:");
+				System.err.println("Argument #1: 'b' for user to play as black, 'w' to play as white. Default is 'b'.");
+				System.err.println("Argument #2: '-tXXXX' to specify the time limit. XXXX is measured in milliseconds. Default is 10000 ms.");
+				System.err.println("Argument #3: 'play' to play against the program, 'auto' to let the program play itself. Default is 'auto'.");
+				System.err.println("Argument #4: 'mm' to use MinMax strategy, 'ab' to add alpha-beta pruning. Default is 'ab'.");
+				System.err.println("Example: Reversi w -t10500 mm play\n");
 				System.exit(1);
 			}
 		}
-		System.out.println("Hi, and welcome to Reversi. You are playing as " +
-				((playerColour == 'b') ? "black" : "white") + ", and I am playing as " +
-				((playerColour == 'w') ? "black" : "white") + ".");
-		System.out.println("My move consideration time limit is " +
-				timeLimit + " ms. Let's play: ");
-		System.out.println(d.draw(board));
 		
+		if (playMode.equals("play")) {
+			System.out.print("Hi, and welcome to Reversi. You are playing as " +
+					((playerColour == 'b') ? "black (X)" : "white (O)") + ", and I am playing as " +
+					((playerColour == 'w') ? "black (X)" : "white (O)") + ".");
+		} else System.out.print("Hi, and welcome to Reversi. I will demonstrate the game by playing vs. myself. Black (X) starts. ");
+		System.out.print("I will be using the MinMax strategy" + ((strategy.equals("ab")) ? " with Alpha-Beta pruning. " : ". "));
+		System.out.println("My move consideration time limit is " + timeLimit + " ms. Let's play: ");
+		
+		System.out.println(d.draw(board));
 		
 		System.out.println("before loop");
 
