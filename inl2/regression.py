8dbf8b140551b11b49c042fc31789ac72458fea6
Halling1 <gustavhalling91@gmail.com>
2017-02-14 13:01:52 +0100

    functioning batch gradient descent

---
+++ inl2/regression.py
@@ -0,0 +1,75 @@
+import sys
+
+####################################################
+#functions
+
+#
+def gradientDescent(w0, w1, data, alfa, epsilon):
+
+	oldSSE = SSE(w0,w1,data)
+	w0,w1 = gradient(w0,w1,data,alfa)
+	print(w0)
+	print(w1)
+	a = 0
+
+	while (abs(SSE(w0,w1,data)-oldSSE) > epsilon):
+		a+=1
+		oldSSE = SSE(w0,w1,data)
+		w0, w1 = gradient(w0,w1,data,alfa)
+		print(w0)
+		print(w1)
+	print("number of iterations: " + str(a))
+	return (w0,w1)
+
+#a step in the direction of the gradient
+def gradient(w0,w1,data,alfa):
+	w0gradient = 0
+	w1gradient = 0
+	n = len(data[0])
+
+	for i in range(n):
+		x = float(values[0][i])
+		y = float(values[1][i])
+		w0gradient += -((2) * (y - (x * w1 + w0)))
+		w1gradient += -((2) * x * (y - (x * w1 + w0)))
+
+	retw0 = w0 - w0gradient*alfa
+	retw1 = w1 - w1gradient*alfa
+	return (retw0,retw1)
+
+#returns sum of sqared errors
+def SSE(w0,w1,values):
+	sse = 0
+	for i in range(len(values[0])):
+		x = float(values[0][i])
+		y = float(values[1][i])
+		sse += (y - (w1 * x + w0)) ** 2
+	return sse
+	
+
+
+#################################################
+#the script
+
+fileName = sys.argv[1]
+f = open(fileName, "r")
+data = f.readlines()
+values = [[], []]
+
+for line in data:
+	values[0].append(line.split()[0])
+	values[1].append(line.split()[1])
+
+#print(values)
+
+
+w0 = 0
+w1 = 0
+alfa = 0.01
+epsilon = 0.000000000001
+
+W = gradientDescent(w0,w1,values,alfa,epsilon)
+print("w0: " + str(W[0]))
+print("w1: " + str(W[1]))
+
+
