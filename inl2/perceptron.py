3036ebafec89f57fb80027122dbb3d556da5c006
Halling1 <gustavhalling91@gmail.com>
2017-02-14 17:18:07 +0100

    implemented reader. Added diabetes data

--- inl2/perceptron.py
+++ inl2/perceptron.py
@@ -7,15 +7,38 @@ import sys
 fileName = sys.argv[1]
 f = open(fileName, "r")
 data = f.readlines()
-
 rows = len(data)
 cols = len(data[0].split())
 
 m = np.zeros(shape=(rows,cols))
 
 for i in range(rows):
+	row = data[i].split()
+	for j in range(cols):
 
+
+		if (j==0):
+
+			m[i,j] = float(row[j])
+		else:
+			a = row[j]
+			a = a[2:]
+			
+			m[i,j] = float(a)
+
+
+#assumes positive  values
+maxVal = np.amax(m)
+print(maxVal)
+if (maxVal>1):
+	scalefactor = 1/maxVal
+	for i in range(rows):
+		for j in range(1,cols):
+			m[i,j] = m[i][j] * scalefactor
+maxVal = np.amax(m)
+print(maxVal)
 print(m)
+
 
 
 #for line in data:
